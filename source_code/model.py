1b7b40970e0f21f6aa35a04827b92dee55fdc994
pislam1289 <89596684+pislam1289@users.noreply.github.com>
2023-12-10 13:06:14 -0500

    Changes Made 12/10/23

--- source_code/model.py
+++ source_code/model.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 class Model:
     def __init__(self):
         self.data = None
@@ -13,9 +12,7 @@ class Model:
     def load_data(self, path_file):
         if path_file.endswith('.WAV'):
             sample_rate, sample_data = wavfile.read(path_file)
-
         else:
-            # here we try to read the file if it is .wav
             try:
                 sample_rate, sample_data = wavfile.read(path_file)
             except ValueError:
@@ -25,58 +22,35 @@ class Model:
 
         self.data = pd.DataFrame({'Channel 1': sample_data})
 
-        if self.data is not None:
-            print(self.data)
-            print("this is channel 1")
-            print(self.data['Channel 1'])
-            print(self.data.info())
-            print(self.data.columns)
-            print(sample_data)
-            print(self.data.head())
-            print(self.data.shape)
-
     def clean_data(self):
-        # Implement self cleaning tool
         self.data = self.data.dropna()
 
-        if 'MetadataColumn' in self.data.columns:
-            self.data = self.data.drop(columns=['MetadataColumn'])
-
     def analyze_data(self):
-        # Generate summary statistics
         summary_stats = self.data.describe()
 
-        # Print summary statistics
-        print("Summary Statistics:")
-        print(summary_stats)
-
-        # Create self visualizations
         self.plot_histogram()
         self.plot_scatter()
         self.plot_sine_wave()
+        self.compute_highest_resonance()
+        self.compute_rt60_for_frequency_ranges()
+        self.calculate_rt60_difference()
 
-        # Identify patterns or trends in RT60 values over three frequency ranges
-        self.identify_rt60_trends()
-
-        # Adjust layout
         plt.tight_layout()
-
-        # Display plots
         plt.show()
 
     def plot_histogram(self):
-        plt.subplot(2, 2, 1)  # 2x2 grid, position 1
+        plt.subplot(2, 2, 1)
         self.data['Channel 1'].plot(kind='hist', title='Histogram of Channel 1')
 
     def plot_scatter(self):
-        plt.subplot(2, 2, 2)  # 2x2 grid, position 2
+        plt.subplot(2, 2, 2)
         plt.scatter(self.data.index, self.data['Channel 1'])
         plt.title('Scatter Plot of Channel 1')
         plt.xlabel('Index')
         plt.ylabel('Channel 1')
 
     def plot_sine_wave(self):
-        plt.subplot(2, 2, 3)  # 2x2 grid, position 3
+        plt.subplot(2, 2, 3)
         x_sine = np.linspace(0, 2 * np.pi, 1000)
         y_sine = np.sin(x_sine)
         plt.plot(x_sine, y_sine, label='Sine Wave')
@@ -85,8 +59,29 @@ class Model:
         plt.ylabel('Y-axis')
         plt.legend()
 
-    def identify_rt60_trends(self):
-        pass
+    def calculate_rt60(self):
+        data = self.data['Channel 1'].to_numpy()
+        time = self.data.index.to_numpy()
+
+        max_db = np.max(data)
+        max_index = np.argmax(data)
+        data_slice = data[max_index:]
+        time_slice = time[max_index:]
+
+        threshold_5db = max_db - 5
+        index_5db = np.argmax(data_slice <= threshold_5db)
+        value_5db = data_slice[index_5db]
+        time_5db = time_slice[index_5db]
+
+        threshold_25db = max_db - 25
+        index_25db = np.argmax(data_slice <= threshold_25db)
+        value_25db = data_slice[index_25db]
+        time_25db = time_slice[index_25db]
+
+        rt20 = time_25db - time_5db
+        rt60 = rt20 * 3
+
+        print("RT60:", rt60)
 
     def visualize_waveform(self):
         plt.plot(self.data.index, self.data['Channel 1'])
@@ -95,81 +90,55 @@ class Model:
         plt.ylabel('Amplitude')
         plt.show()
 
+    def compute_highest_resonance(self):
+        if self.data is not None:
+            data = self.data['Channel 1'].to_numpy()
+            sample_rate = 1 / (self.data.index[1] - self.data.index[0])
+            fft_result = np.fft.fft(data)
+            frequencies = np.fft.fftfreq(len(fft_result), 1 / sample_rate)
+            magnitude = np.abs(fft_result)
+            peak_freq_index = np.argmax(magnitude)
+            highest_resonance_freq = frequencies[peak_freq_index]
+            print("Highest Resonance Frequency:", highest_resonance_freq, "Hz")
 
-        # all the commented below was me trying to find the
-        # RT60 but no luck
+    def compute_rt60_for_frequency_ranges(self):
+        if self.data is not None:
+            data = self.data['Channel 1'].to_numpy()
+            sample_rate = 1 / (self.data.index[1] - self.data.index[0])
+            frequencies = np.fft.fftfreq(len(data), 1 / sample_rate)
 
-"""
-    def calculate_rt60(self):
+            low_freq_range = (20, 200)
+            mid_freq_range = (200, 2000)
+            high_freq_range = (2000, 20000)
 
-        data = self.data['Channel 1'].to_numpy()
-        time = self.data.index.to_numpy()
+            rt60_low = self.compute_rt60_for_range(data, frequencies, low_freq_range)
+            rt60_mid = self.compute_rt60_for_range(data, frequencies, mid_freq_range)
+            rt60_high = self.compute_rt60_for_range(data, frequencies, high_freq_range)
 
-        # Step 1: Find Maximum value of dB's in array
-        max_db = np.max(data)
+            print("RT60 for Low Frequencies:", rt60_low, "s")
+            print("RT60 for Mid Frequencies:", rt60_mid, "s")
+            print("RT60 for High Frequencies:", rt60_high, "s")
 
-        # Step 2: Slice self and time arrays to location of maximum value
-        max_index = np.argmax(data)
-        data_slice = data[max_index:]
-        time_slice = time[max_index:]
+    def compute_rt60_for_range(self, data, frequencies, freq_range):
+        freq_mask = (frequencies >= freq_range[0]) & (frequencies <= freq_range[1])
+        filtered_data = data * freq_mask
+        self.data['Channel 1'] = filtered_data  # Update data to the filtered range
+        self.calculate_rt60()  # Recalculate RT60 for the filtered range
+        return self.data['Channel 1']
 
-        # Step 3: Find value which is Max - 5dB
-        threshold_5db = max_db - 5
-        index_5db = np.argmax(data_slice <= threshold_5db)
-        value_5db = data_slice[index_5db]
-        time_5db = time_slice[index_5db]
-
-        # Step 4: Find a value which is equal to max - 25dB
-        threshold_25db = max_db - 25
-        index_25db = np.argmax(data_slice <= threshold_25db)
-        value_25db = data_slice[index_25db]
-        time_25db = time_slice[index_25db]
-
-        # Step 5: Calculate RT20 as time it takes amplitude to drop from max (less 5dB) to max (less 25dB)
-        rt20 = time_25db - time_5db
-
-        # Step 6: Multiply times by 3 to give RT60 reverb time
-        rt60 = rt20 * 3
-
-        return rt60
-
-    def identify_rt60_trends(self):
-        # Assume RT60 values are in a column named 'RT60'
-        rt60_values = self.self['RT60']
-
-        # Boxplot of RT60 values
-        plt.subplot(2, 2, 4)  # 2x2 grid, position 4
-        rt60_values.plot(kind='box', title='Boxplot of RT60 Values')
-
-        # Bar chart of RT60 values for each frequency range
-        plt.subplot(2, 2, 4)  # 2x2 grid, position 4 (or choose a new position)
-        self.plot_rt60_bar_chart()
-
-    def plot_rt60_bar_chart(self):
-        # Assuming self has columns 'LowFreq_RT60', 'MidFreq_RT60', 'HighFreq_RT60'
-        rt60_freq_ranges = ['LowFreq_RT60', 'MidFreq_RT60', 'HighFreq_RT60']
-        rt60_values = self.self[rt60_freq_ranges]
-        rt60_values.plot(kind='bar', stacked=True, title='RT60 Values Over Three Frequency Ranges')
-
-    def analyze_data(self):
-        # Implement self analysis methods
-        summary_stats = self.self.describe()
-
-        # Print summary statistics
-        print("Summary Statistics:")
-        print(summary_stats)
-
-        # Create self visualizations (you can customize based on your requirements)
-        # Example: Histogram
-        #self.self['Channel 1'].plot(kind='hist', title='Histogram of Channel 1')
-
-        # Example: Scatter Plot
-        # self.self.plot.scatter(x='Channel 1', y='SomeOtherColumn', title='Scatter Plot')
+    def calculate_rt60_difference(self):
+        if self.data is not None:
+            target_rt60 = 0.5
+            self.calculate_rt60()  # Recalculate RT60 based on the current data
+            original_rt60 = self.data['Channel 1'].to_numpy()
+            rt60_difference = original_rt60 - target_rt60
+            print("RT60 Difference (Original - Target):", rt60_difference, "s")
+        else:
+            print("No data loaded.")
 
 
-
-        # Identify patterns or trends in the self
-        # (This part depends on your specific analysis requirements)"""
-
-
-
+if __name__ == "__main__":
+    model = Model()
+    model.load_data("your_audio_file.wav")
+    model.clean_data()
+    model.analyze_data()
--- source_code/view.py
+++ source_code/view.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 from model import Model
+import matplotlib.pyplot as plt
 
 
 class App:
@@ -18,6 +19,10 @@ class App:
         self.show_plots_button = tk.Button(master, text="Show Plots", command=self.show_plots)
         self.show_plots_button.pack()
 
+        # Combine Plots Button
+        self.combine_plots_button = tk.Button(master, text="Combine Plots", command=self.combine_plots)
+        self.combine_plots_button.pack()
+
     def load_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav;*.mp3;*.m4a")])
         if file_path:
@@ -26,9 +31,21 @@ class App:
 
     def show_plots(self):
         if self.model_instance.data is not None:
-            #self.model_instance.clean_data()
+            # self.model_instance.clean_data()
             self.model_instance.analyze_data()
         else:
             print("Please load a file first.")
 
-
+    def combine_plots(self):
+        if self.model_instance.data is not None:
+            plt.figure(figsize=(12, 6))
+            plt.subplot(2, 2, 1)
+            self.model_instance.plot_histogram()
+            plt.subplot(2, 2, 2)
+            self.model_instance.plot_scatter()
+            plt.subplot(2, 2, 3)
+            self.model_instance.plot_sine_wave()
+            plt.tight_layout()
+            plt.show()
+        else:
+            print("Please load a file first.")
